4b51b2ec9e24f318bb7dd4c1049906131d9423f6
SidneyLi02 <66651189+SidneyLi02@users.noreply.github.com>
2021-01-05 02:34:11 -0800

    finished html/css
    - chrome disables autoplay, must use edge
    - javascript broken

--- scripts/run.js
+++ scripts/run.js
@@ -1,1 +1,22 @@
-let foo = 0;
+const Game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    const initGame = () => {
+
+        const initBtn = document.querySelector(".intro button");
+        const initScreen = document.querySelector(".intro");
+        const match = document.querySelector("match");
+        
+        // if initBtn is clicked, add fadeOut to class of intro page
+        initBtn.addEventListener("click", () => {
+            initScreen.classList.add("fadeOut");
+        });
+
+    };
+
+    initGame();
+
+};
+
+Game();
