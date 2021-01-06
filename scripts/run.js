6fc44e2034cc8ed9d9da0d581e12f3155e9e6f27
SidneyLi02 <66651189+SidneyLi02@users.noreply.github.com>
2021-01-05 23:13:24 -0800

    small edits

--- scripts/run.js
+++ scripts/run.js
@@ -3,6 +3,7 @@ const Game = () => {
     let playerScore = 0;
     let computerScore = 0;
 
+    // init game
     const initGame = () => {
 
         const initBtn = document.querySelector(".intro button");
@@ -12,12 +13,31 @@ const Game = () => {
         // if initBtn is clicked, add fadeOut to class of intro page
         initBtn.addEventListener("click", () => {
             initScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
         });
-
     };
+    
+    // play rps match
+    const playRPS = () => {
 
+        const playerOption = document.querySelectorAll(".option button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+
+        // computer option
+        const computerOptions = ["rock", "paper", "scissors"];
+
+        const indexSelector = (maxInt) => {
+            return Math.floor(Math.random() * Math.floor(maxInt));
+        }
+        console.log(indexSelector(2));
+    };
+    playRPS();
+
+    // transition 
     initGame();
 
 };
 
+// init script
 Game();
