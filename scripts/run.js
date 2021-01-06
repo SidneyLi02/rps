2f4dfe805e6dfa74e6d0d14e1c83bf77af280c9b
SidneyLi02 <66651189+SidneyLi02@users.noreply.github.com>
2021-01-05 23:47:34 -0800

    update img

--- scripts/run.js
+++ scripts/run.js
@@ -20,22 +20,72 @@ const Game = () => {
     // play rps match
     const playRPS = () => {
 
-        const playerOption = document.querySelectorAll(".option button");
+        const options = document.querySelectorAll(".option button");
         const playerHand = document.querySelector(".player-hand");
         const computerHand = document.querySelector(".computer-hand");
 
         // computer option
         const computerOptions = ["rock", "paper", "scissors"];
 
+        // returns [0, maxInt-1] where maxInt >= 1 
         const indexSelector = (maxInt) => {
             return Math.floor(Math.random() * Math.floor(maxInt));
-        }
-        console.log(indexSelector(2));
-    };
-    playRPS();
+        };
+        
+        // for each element within options (rock, paper, scissors), assign a computer option
+        options.forEach(option => {
+            option.addEventListener("click", function() {
+                const computerChoice = computerOptions[indexSelector(3)];
 
+                // determine game conditions
+                compareHands(this.textContent, computerChoice);
+
+                // update images of pick
+                // note the backtick, $ indicates variable
+                playerHand.src = `assets/img/${this.textContent}.png`;
+                computerHand.src = `assets/img/${computerChoice}.png`;
+            });
+        });
+    };
+
+    const compareHands = (playerC, computerC) => {
+
+        // update top text 
+        const winner = document.querySelector(".winner");
+
+        // tie
+        if (playerC === computerC) {
+            winner.textContent = "Tie"
+            return;
+        }
+
+        // winning conditions
+        else if (playerC === "rock" && computerC === "scissors") {
+            winner.textContent = "You win"
+            return;
+        }
+
+        else if (playerC === "paper" && computerC === "rock") {
+            winner.textContent = "You win"
+            return;
+        }
+
+        else if (playerC === "scissors" && computerC === "paper") {
+            winner.textContent = "You win"
+            return;
+        }
+
+        // catch for loss
+        else {
+            winner.textContent = "You lose"
+            return;
+        }
+
+    };
+    
     // transition 
     initGame();
+    playRPS();
 
 };
 
